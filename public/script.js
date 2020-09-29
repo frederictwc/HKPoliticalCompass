14fe7d5ac6e265883c62e882acdea0af5c32a5ec
frederictwc <frederictwc@live.com>
2020-09-29 21:39:10 +0800

    simplified

--- public/script.js
+++ public/script.js
@@ -21,50 +21,21 @@
 		}
 
 	// calc score with answerScore function
-		var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-		console.log("CalcScore: " + calcScore); // it works!
-
-	// function to return correct answer string
-		function correctAnswer (correctStringNo, qNumber) {
-			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-			return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
-				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
-			}
-
-	// print correct answers only if wrong (calls correctAnswer function)
-		if (answerScore('q1') === 0) {
-			document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-		}
-		if (answerScore('q2') === 0) {
-			console.log(document.getElementById('hk_gov_logo'))
-			console.log('hi')
-			document.getElementById('hk_gov_logo').style.display = 'block'
-			console.log(document.getElementById('hk_gov_logo'))
-			document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-		    //document.getElementById('hk_gov_logo').style.display == 'block';
-		}
+		var calcScore = (answerScore('q1') + answerScore('q2'));
 
 	// calculate "possible score" integer
 		var questionCountArray = document.getElementsByClassName('question');
-
 		var questionCounter = 0;
 		for (var i = 0, length = questionCountArray.length; i < length; i++) {
 			questionCounter++;
 		}
-
 	// show score as "score/possible score"
-		var showScore = "Your Score: " + calcScore +"/" + questionCounter;
-	// if 4/4, "perfect score!"
-		if (calcScore === questionCounter) {
-			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-		};
+		var showScore = "Your political score: " + calcScore +"/" + questionCounter;
+
 		document.getElementById('userScore').innerHTML = showScore;
+		document.getElementById('hk_gov_logo').style.display = 'block'
+
+        console.log("Total Score: " + calcScore + ", " + '# Questions: ' + questionCounter )
+
 	}
 
-$(document).ready(function() {
-
-	$('#submitButton').click(function() {
-		$(this).addClass('hide');
-	});
-
-});
