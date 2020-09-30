884243da58269749f9461ffff0b23252bd6abbcf
frederictwc <frederictwc@live.com>
2020-10-01 00:30:29 +0800

    database works

--- public/script.js
+++ public/script.js
@@ -75,12 +75,17 @@
 			  messagingSenderId: "883847575366",
 			  appId: "1:883847575366:web:82632bd8f78b0600b65747",
 			  measurementId: "G-EFE7F08XT6"};
-	    firebase.initializeApp(firebaseConfig);
-	    function writeData() {
-	        	firebase.database().ref("Answers").set({
+	    if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+	    function writeData(questionCounter,calcScore) {
+	    	    var now = new Date()
+	        	firebase.database().ref(now.getTime()).set({
+             
 	        		NoQuestions: questionCounter,
 	        		Score: calcScore
 	        	});
 	        }
-        writeData()
+        writeData(questionCounter,calcScore)
+
 	}
